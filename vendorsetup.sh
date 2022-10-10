d0f6fb20fcc383ea7ca75f51becfbb308cdbefec
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2022-10-10 23:01:34 +0530

    spes: Update vendorsetup
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
 # Hardware
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-19.1 hardware/xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
 
 # Kernel
-git clone --depth=1 https://github.com/Deepak5310/android_kernel_xiaomi_spes.git -b 12.1 kernel/xiaomi/spes
+git clone --depth=1 https://github.com/Deepak5310/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
 
 # Remove pixel headers to avoid conflicts
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Vendor
-git clone --depth=1 https://github.com/Jabiyeff/android_vendor_xiaomi_spes.git -b 12.1 vendor/xiaomi/spes
+git clone --depth=1 https://github.com/Jabiyeff/android_vendor_xiaomi_spes.git -b 13.0 vendor/xiaomi/spes
