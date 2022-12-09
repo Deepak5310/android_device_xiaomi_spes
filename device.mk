39cb087f176057372adab4f494e5f37ff4f6c58b
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2022-12-10 01:34:01 +0530

    Revert "spes: Use component-override to configure Bluetooth"
    This reverts commit 803a7c7451db186bfa85437e9eec482d8b2d2c98.
    
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -166,11 +166,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
-    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
-
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
