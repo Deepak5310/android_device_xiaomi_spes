cea59377e0952f93ff1cc2ab9e478e3ceb6b34ea
TheStrechh <TheStrechh@gmail.com>
2022-12-19 02:30:13 +0000

    spes: Set correct hardware soong
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -391,10 +391,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
     android.hardware.power-service.xiaomi-libperfmgr
 
-PRODUCT_SOONG_NAMESPACES += \
-    hardware/google/interfaces \
-    hardware/google/pixel
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
@@ -459,6 +455,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi
 
 # Speed profile services and wifi-service to reduce RAM and storage
